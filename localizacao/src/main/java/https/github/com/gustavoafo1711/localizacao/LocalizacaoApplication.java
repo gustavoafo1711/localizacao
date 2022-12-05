29382ea8dcf0a3950bb6f13157fc9c417c0b795a
gustavo <gustavoafo@hotmail.com>
2022-12-05 18:53:04 -0300

    Testando o repositório de Cidades

--- localizacao/src/main/java/https/github/com/gustavoafo1711/localizacao/LocalizacaoApplication.java
+++ localizacao/src/main/java/https/github/com/gustavoafo1711/localizacao/LocalizacaoApplication.java
@@ -1,16 +1,34 @@
 package https.github.com.gustavoafo1711.localizacao;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import https.github.com.gustavoafo1711.localizacao.domain.entity.Cidade;
+import https.github.com.gustavoafo1711.localizacao.domain.repository.CidadeRepository;
+import jakarta.transaction.Transactional;
+
 @SpringBootApplication
 public class LocalizacaoApplication implements CommandLineRunner{
 
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
-		
-		
+		salvarCidade();
+		listarCidades();
+	}
+	
+	@Transactional
+	void salvarCidade () {
+		var cidade = new Cidade(1L, "São Paulo", 12000000L);
+		cidadeRepository.save(cidade);
+	}
+	
+	void listarCidades() {
+		cidadeRepository.findAll().forEach(System.out::println);
 	}
 
 	public static void main(String[] args) {
---
+++ localizacao/src/main/java/https/github/com/gustavoafo1711/localizacao/domain/repository/CidadeRepository.java
@@ -0,0 +1,9 @@
+package https.github.com.gustavoafo1711.localizacao.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import https.github.com.gustavoafo1711.localizacao.domain.entity.Cidade;
+
+public interface CidadeRepository extends JpaRepository<Cidade, Long>{
+
+}
